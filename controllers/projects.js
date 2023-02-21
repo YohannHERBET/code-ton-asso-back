2c1a5f603a565ba0c6b81e15be6d978ce391b786
Marilène <67200994+MarileneK@users.noreply.github.com>
2023-02-21 15:10:21 +0100

    feat | Adjusting /projects/:slug route

--- controllers/projects.js
+++ controllers/projects.js
@@ -7,17 +7,20 @@ const getProjects = async (req, res) => {
     return res.status(404).send({ message: 'Aucun projet trouvé' });
   }
   res.json(projects);
-}
+};
 
 const getProject = async (req, res) => {
+  console.log('req.params', req.params);
   const project = await Project.findOne({
-    where: { id: req.params.id },
+    where: { slug: req.params.slug },
   });
   if (!project) {
-    return res.status(404).send({ message: 'La fonctionnalité n\'a pas été trouvée' });
+    return res
+      .status(404)
+      .send({ message: "La fonctionnalité n'a pas été trouvée" });
   }
   res.json(project);
-}
+};
 
 const createProject = async (req, res) => {
   try {
@@ -29,19 +32,21 @@ const createProject = async (req, res) => {
     }
     res.status(500).send({ message: 'Erreur interne' });
   }
-}
+};
 
 const updateProject = async (req, res) => {
-  const project = await Project.findOne({ where: { id: req.params.id }});
+  const project = await Project.findOne({ where: { id: req.params.id } });
   if (!project) {
-    return res.status(404).send({ message: 'La fonctionnalité n\'a pas été trouvée'});
+    return res
+      .status(404)
+      .send({ message: "La fonctionnalité n'a pas été trouvée" });
   }
   await Project.update(req.body, { where: { id: req.params.id } });
   res.json(project);
-}
+};
 
 const deleteProject = async (req, res) => {
-  const project = await Project.findOne({ where: { id: req.params.id }});
+  const project = await Project.findOne({ where: { id: req.params.id } });
   if (user) {
     await Project.destroy({
       where: { id: req.params.id },
@@ -49,7 +54,7 @@ const deleteProject = async (req, res) => {
     return res.send({ message: 'Projet supprimé' });
   }
   res.status(404).send({ message: 'Projet non trouvé' });
-}
+};
 
 module.exports = {
   getProjects,
