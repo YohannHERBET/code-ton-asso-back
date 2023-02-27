a48d129aae05bd121ba8239257bf0f791f69809b
Marilène <67200994+MarileneK@users.noreply.github.com>
2023-02-27 11:12:04 +0100

    fix: getting details of a single association

--- controllers/associations.js
+++ controllers/associations.js
@@ -1,29 +1,39 @@
-const { Op } = require("sequelize");
+const { Op } = require('sequelize');
 
 const db = require('../models');
 const { Association, User } = db;
 
 const getAssociations = async (req, res) => {
-  const associations = await Association.findAll();
+  const associations = await Association.findAll({
+    include: [{ all: true, nested: true }],
+  });
   if (!associations) return res.status(404).send('Aucune association trouvée.');
   res.json(associations);
-}
+};
 
 const getLatestAssociations = async (req, res) => {
   const associations = await User.findAll({
     limit: 3,
-    where : { association_id: { [Op.ne]: null } },
+    where: { association_id: { [Op.ne]: null } },
     order: [['createdAt', 'DESC']],
     include: ['association'],
   });
   if (!associations) return res.status(404).send('Aucune association trouvée.');
   res.json(associations);
-}
+};
 
 const getAssociation = async (req, res) => {
-  const association = await Association.findOne({ where: { id: req.params.id } });
+  const association = await Association.findOne({
+    where: { slug: req.params.slug },
+    include: [{ all: true, nested: true }],
+  });
+  if (!association) {
+    return res
+      .status(404)
+      .send({ message: "L'association n'a pas été trouvée" });
+  }
   res.json(association);
-}
+};
 
 const createAssociation = async (req, res) => {
   try {
@@ -33,19 +43,23 @@ const createAssociation = async (req, res) => {
     if (error.name.includes('Sequelize')) {
       return res.status(400).send(error.errors.map((err) => err.message));
     }
-    res.status(500).send({ message: 'Erreur interne' });    
+    res.status(500).send({ message: 'Erreur interne' });
   }
-}
+};
 
 const updateAssociation = async (req, res) => {
-  const association = await Association.findOne({ where: { id: req.params.id } });
+  const association = await Association.findOne({
+    where: { id: req.params.id },
+  });
   if (!association) return res.status(404).send('Association non trouvée.');
   await Association.update(req.body, { where: { id: req.params.id } });
   res.json(association);
-}
+};
 
 const deleteAssociation = async (req, res) => {
-  const association = await Association.findOne({ where: { id: req.params.id } });
+  const association = await Association.findOne({
+    where: { id: req.params.id },
+  });
   if (association) {
     await Association.destroy({
       where: { id: req.params.id },
@@ -53,7 +67,7 @@ const deleteAssociation = async (req, res) => {
     return res.send({ message: 'Association supprimée' });
   }
   res.status(404).send({ message: 'Association non trouvée' });
-}
+};
 
 module.exports = {
   getAssociations,
--- routes/associations.js
+++ routes/associations.js
@@ -12,6 +12,7 @@ const {
 } = require('../controllers/associations');
 
 router.get('/', getAssociations);
+router.get('/:slug', getAssociation);
 router.get('/latest', getLatestAssociations);
 router.get('/:id', getAssociation);
 router.put('/:id', auth, updateAssociation);
